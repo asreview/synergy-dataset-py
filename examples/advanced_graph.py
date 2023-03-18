08d6e9d71b4bfdc6c71ce9e2c565f67caad4007f
Jonathan de Bruin <jonathandebruinos@gmail.com>
2023-03-18 10:14:11 +0100

    Simplify advanced code example

--- examples/advanced_graph.py
+++ examples/advanced_graph.py
@@ -1,31 +1,16 @@
-# download networkx (pip install networkx) if not available
 import networkx as nx
 
 from pyodss import Dataset
 
-dataset_name = "Appenzeller-Herzog_2020"
+# load dataset
+d = Dataset("Appenzeller-Herzog_2020")
+result = d.to_dict(["id", "title", "referenced_works"])
 
-# load dataset with referenced_works
-d = Dataset(dataset_name)
-result = d.to_dict(
-    variables={
-        "title": lambda x: x["title"],
-        "referenced_works": lambda x: x["referenced_works"],
-    }
-)
-
-# make a graph of the dataset with networkx
+# make collection of nodes and edges
 nodes = [(k, {"label_included": v["label_included"]}) for k, v in result.items()]
-edges = [
-    (k, r)
-    for k, v in result.items()
-    if "referenced_works" in v
-    for r in v["referenced_works"]
-]
+edges = [(k, r) for k, v in result.items() for r in v["referenced_works"]]
 
-print("Number of nodes", len(nodes))
-print("Number of edges", len(edges))
-
+# build the graph
 G = nx.Graph()
 G.add_nodes_from(nodes)
 G.add_edges_from(edges)
@@ -34,4 +19,4 @@ G.remove_nodes_from(set(G.nodes) - set([n[0] for n in nodes]))
 print("Number of nodes", len(G.nodes))
 print("Number of edges", len(G.edges))
 
-nx.write_gexf(G, f"{dataset_name}_network.gexf")
+nx.write_gexf(G, f"Appenzeller-Herzog_2020_network.gexf")
