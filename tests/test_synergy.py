5372c0de8c11f204a030618e7637fa69267ee624
Jonathan de Bruin <jonathandebruinos@gmail.com>
2023-04-25 19:18:47 +0200

    Add unit test

--- tests/test_synergy.py
+++ tests/test_synergy.py
@@ -1,3 +1,5 @@
+import pytest
+
 from synergy_dataset import Dataset
 from synergy_dataset import iter_datasets
 
@@ -10,3 +12,12 @@ def test_build_dataset():
     d = Dataset("van_de_Schoot_2018")
 
     d.to_dict()
+
+
+@pytest.mark.parametrize("dataset_name", [("Walker_2018",), ("van_de_Schoot_2018",)])
+def test_order_iter_dataset(dataset_name):
+    d = Dataset(dataset_name)
+
+    keys = [k for k, v in d.to_dict().items()]
+
+    assert keys == list(d.labels.keys())
