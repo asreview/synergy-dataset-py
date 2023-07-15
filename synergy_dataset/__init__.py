73da9a3c662a6c6455c5c23c208a5fe2be98ebe2
Jonathan de Bruin <jonathandebruinos@gmail.com>
2023-07-15 17:04:39 +0200

    Add option to load Dataset from custom path

--- synergy_dataset/__init__.py
+++ synergy_dataset/__init__.py
@@ -2,6 +2,7 @@ from synergy_dataset._version import __version__  # noqa
 from synergy_dataset._version import __version_tuple__  # noqa
 from synergy_dataset.base import Dataset
 from synergy_dataset.base import download_raw_dataset
+from synergy_dataset.base import download_raw_subset
 from synergy_dataset.base import iter_datasets
 
-__all__ = ["Dataset", "download_raw_dataset", "iter_datasets"]
+__all__ = ["Dataset", "download_raw_dataset", "download_raw_subset", "iter_datasets"]
